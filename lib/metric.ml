1ed418036261b82b36116169568f63ac817b43d5
Ilias <ilias.gar@gmail.com>
2018-10-13 12:06:38 +0200

    added pullbacks of metric spaces

--- lib/metric.ml
+++ lib/metric.ml
@@ -16,3 +16,15 @@ module type OrderedS =
     include S
     val compare : t -> t -> int
   end
+
+let pullback (type a b) (f : a -> b) (met : (module S with type t = b)) =
+  let module Met = (val met) in
+  let module PbMet =
+  struct
+    type t = a
+
+    let dist x y =
+      Met.dist (f x) (f y)
+  end
+  in
+  (module PbMet : S with type t = a)
--- lib/metric.mli
+++ lib/metric.mli
@@ -1,18 +1,24 @@
+(** (Pseudo-)Metric spaces. The axioms are not explicitly enforced. *)
 module type S =
   sig
     type t
     val dist : t -> t -> float
   end
 
+(** Finite (pseudo-)metric spaces. Elements are explicitly stored in an array. *)
 module type FiniteS =
   sig
     include S
     val elements : t array
   end
 
+(** Ordered (pseudo-)metric spaces. Useful to construct spaces of maps, etc. *)
 module type OrderedS =
   sig
     include S
 
     val compare : t -> t -> int
   end
+
+(** Computes the pullback of a (pseudo) metric space along a map. *)
+val pullback : ('a -> 'b) -> (module S with type t = 'b) -> (module S with type t = 'a)
